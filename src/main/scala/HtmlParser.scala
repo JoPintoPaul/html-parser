976cff28e26ed8678f552a2c2370bfdf73861262
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-03-24 12:32:56 +0000

    Updated POC parser

--- src/main/scala/HtmlParser.scala
+++ src/main/scala/HtmlParser.scala
@@ -4,9 +4,13 @@ import scala.util.matching.Regex
 
 object HtmlParser {
   def main(args: Array[String]): Unit = {
-    val messagesPath = ""
-    val htmlPath = ""
-    val templatePath = ""
+    val htmlPath = "page-source-en.txt"
+    val htmlPathCy = "page-source-cy.txt"
+
+    val messagesPath = "messages"
+    val messagesPathCy = "messages-cy"
+
+    val templatePath = "OnlineTermsPage.txt"
 
     val inputLines: Seq[String] = getLines(htmlPath)
     val parsedLines: Seq[ParsedLine] = parseLines(inputLines)
@@ -16,6 +20,11 @@ object HtmlParser {
 
     val messageLines: Seq[String] = parsedLines.flatMap(_.message.map(_.toString))
     writeLinesToFile(messagesPath, messageLines)
+
+    val inputLinesCy: Seq[String] = getLines(htmlPathCy)
+    val parsedLinesCy: Seq[ParsedLine] = parseLines(inputLinesCy)
+    val messageLinesCy = parsedLinesCy.flatMap(_.message.map(_.toString))
+    writeLinesToFile(messagesPathCy, messageLinesCy)
   }
 
   private def getLines(path: String): List[String] = {
@@ -27,18 +36,34 @@ object HtmlParser {
 
   private def parseLines(inputLines: Seq[String]): Seq[ParsedLine] = {
     val paragraphRegex: Regex = "(<p>)(.*)(</p>)".r
+    val headingLargeRegex: Regex = "<h3 id=\"(.*)\">(.*)</h3>".r
+    val headingMediumRegex: Regex = "<h4(.*)?>(.*)</h4>".r
+
     inputLines map {
       case paragraph@paragraphRegex(_, innerText, _) =>
-        extractMessage(innerText) match {
+        extractParagraphMessage(innerText) match {
           case Some(message) =>
             ParsedLine("<p class=\"govuk-body\">" + message.toHtml() + "<p>", Some(message))
           case _ => ParsedLine(paragraph, None)
+        }
+      case headingLarge@headingLargeRegex(id, innerText) =>
+        extractHeaderMessage(innerText, Some(id)) match {
+          case Some(message) =>
+            ParsedLine(s"<h3 id=\"$id\" class=\"govuk-heading-l\">" + message.toHtml() + "<h3>", Some(message))
+          case _ => ParsedLine(headingLarge, None)
+        }
+
+      case headingMedium@headingMediumRegex(id, innerText) =>
+        extractHeaderMessage(innerText, None) match {
+          case Some(message) =>
+            ParsedLine(s"<h4${id} class=\"govuk-heading-m\">" + message.toHtml() + "<h4>", Some(message))
+          case _ => ParsedLine(headingMedium, None)
         }
       case line => ParsedLine(line, None)
     }
   }
 
-  private def extractMessage(innerText: String): Option[Message] = {
+  private def extractParagraphMessage(innerText: String): Option[Message] = {
     val indexRegex = "[1-9]+(\\.)[1-9]+".r
     indexRegex.findFirstIn(innerText) match {
       case Some(index) =>
@@ -46,6 +71,14 @@ object HtmlParser {
         Some(Message(messagesKey, innerText))
       case None => None
     }
+  }
+
+  private def extractHeaderMessage(innerText: String, id: Option[String]): Option[Message] = {
+    val messagesKey = id match {
+      case Some(i) => s"heading.$i"
+      case None => s"heading.${innerText.toLowerCase.replace(" ", "-").replace(",", "")}"
+    }
+    Some(Message(messagesKey, innerText))
   }
 
   private def writeLinesToFile(path: String, lines: Seq[String]) = {
@@ -61,6 +94,6 @@ object HtmlParser {
   case class Message(key: String, value: String) {
     override def toString: String = s"$key=$value"
 
-    def toHtml(): String = s"@messages(\"${key}\")"
+    def toHtml(): String = s"@messages(\"$key\")"
   }
 }
